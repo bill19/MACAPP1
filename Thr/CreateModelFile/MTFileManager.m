2d4e0f8167e6502384c69e132282eb505cfb6c67
HaoSun <sunhao.private@foxmail.com>
2017-11-10 13:18:54 +0800

    暂时完结程序
    算是写完了，好多东西还得继续优化，一会出个文档，我会把所有的东西从新优化一下

--- Thr/CreateModelFile/MTFileManager.m
+++ Thr/CreateModelFile/MTFileManager.m
@@ -77,8 +77,10 @@
 
     NSDictionary *dict = [self mergeParms:modelArray];
     for (NSString *url in self.urls) {
+        /**插入方法信息*/
         NSMutableString *tempSourceString = [NSMutableString string];
         NSArray *models = dict[url];
+        [tempSourceString appendString: [self addMarkModels:models]];
         [tempSourceString appendString:@"+ (void)"];
         [tempSourceString appendString:@"request"];
         [tempSourceString appendString:[_abString uppercaseString]];
@@ -100,9 +102,69 @@
     NSString *str1 = [NSString stringWithFormat:@"%@Request",_className];
     [self.headerString appendFormat:k_CLASS,str1,sourceString];
 
+    [self creatRequestFileSourceWithArray:modelArray];
     return  [self generateFileAllowForHeader:@"Request"];
 }
 
+- (BOOL)creatRequestFileSourceWithArray:(NSArray <SHParmsModel *>*)modelArray {
+
+    self.sourceString = [NSMutableString string];
+    [self creatFileHeaderSource];
+    NSMutableString *sourceString = [NSMutableString string];
+    /**创建唯一调用方法*/
+    [sourceString appendString:@"+ (void)judgeReturnValueResponseObject:(id)responseObject Success:(void(^)())success fauile:(void(^)())fauile{\nif (responseObject != nil || [responseObject isKindOfClass:[NSDictionary class]]) {\nNSDictionary *parm = (NSDictionary *)responseObject;\nif ([parm[@\"success\"] isEqualToString:@\"true\"]) {\nsuccess();\n} else {\nfauile();\n}\n}\n}\n\n"];
+    NSDictionary *dict = [self mergeParms:modelArray];
+    for (NSString *url in self.urls) {
+        /**插入方法信息*/
+        NSMutableString *tempSourceString = [NSMutableString string];
+        NSArray *models = dict[url];
+        [tempSourceString appendString: [self addMarkModels:models]];
+        [tempSourceString appendString:@"+ (void)"];
+        [tempSourceString appendString:@"request"];
+        [tempSourceString appendString:[_abString uppercaseString]];
+        [tempSourceString appendString:[[url componentsSeparatedByString:@"/"] lastObject]];
+
+        NSMutableString *tempUrl = [NSMutableString string];
+        NSString *temStruUrl = [[url componentsSeparatedByString:@"/"] lastObject];
+        [tempUrl appendFormat:@"URL_%@_%@ ",[self.abString uppercaseStringWithLocale:[NSLocale currentLocale]],[temStruUrl uppercaseStringWithLocale:[NSLocale currentLocale]]];
+
+        NSMutableString *inputSourceString = [NSMutableString string];
+        NSString *requestType = [NSString string];
+        [inputSourceString appendString:@"NSMutableDictionary *params = [NSMutableDictionary dictionary];\n"];
+        for (int i = 0; i < models.count; i++) {
+            //参数对接.h文件
+            SHParmsModel *parmModel = [models objectAtIndex:i];
+            [tempSourceString appendString:parmModel.netParameterName];
+            [tempSourceString appendString:@":"];
+            [tempSourceString appendString:[self parmType:parmModel.netTypeName]];
+            [tempSourceString appendString:parmModel.netParameterName];
+            [tempSourceString appendString:@" "];
+            //参数新增到内部
+            NSMutableString *tempType =[NSMutableString string];
+            if ([parmModel.netTypeName isEqualToString:@"NSString"]) {
+                [tempType appendString:parmModel.netParameterName];
+            }else{
+                [tempType appendString:[self addParentheses:parmModel.netParameterName]];
+            }
+            [inputSourceString appendFormat:@"[self addParmWith:params mtobject:%@ mtkey:@\"%@\"];\n",tempType,parmModel.netParameterName];
+            requestType = parmModel.netRequest;
+        }
+        [tempSourceString appendString:[self appendBlockInfoSource]];
+        [inputSourceString appendString:@"[self addParametersForDict:params];\n"];
+        [inputSourceString appendString:[self addUnifiedUrl:tempUrl requestType:[requestType uppercaseString]]];
+        inputSourceString = [self addBrackets:inputSourceString];
+        [tempSourceString appendString:inputSourceString];
+        [tempSourceString appendString:@"\n\n"];
+        [sourceString appendString:tempSourceString];
+        [sourceString appendString:@"\n"];
+    }
+
+    [sourceString appendString:@"/**统一增加某些参数 例如 userid 等*/\n+ (void)addParametersForDict:(NSMutableDictionary *)dict {\n[self addParmWith:dict mtobject:@\" mtkey:@\"\"];\n}\n"];
+    [sourceString appendString:@"/**增加参数方法*/\n + (void)addParmWith:(NSMutableDictionary *)dict mtobject:(id)object mtkey:(NSString *)key{\nif ([key isKindOfClass:[NSString class]]) {\nif (object && key.length) {\n[dict setObject:object forKey:key];\n}\n}\n}\n"];
+    NSString *str1 = [NSString stringWithFormat:@"%@Request",_className];
+    [self.sourceString appendFormat:k_CLASS_M,str1,sourceString];
+    return YES;
+}
 
 /**
  创建文件的头信息
@@ -113,9 +175,23 @@
     [self.headerString appendFormat:k_HEADINFO('h'),_className,_projectName,_developerName,dateStr,dateStr2,_developerName];
     [self.headerString appendString:@"\n\n"];
 }
+/**
+ 创建.m文件的头信息
+ */
+- (void)creatFileHeaderSource {
+    NSString *dateStr = [NSDate stringWithFormat:@"yyyy/MM/dd"];
+    NSString *dateStr2 = [NSDate stringWithFormat:@"yyyy"];
+    NSString *classNameStr = [NSString stringWithFormat:@"%@Request",_className];
+    [self.sourceString appendFormat:k_HEADINFO('m'),classNameStr,_projectName,_developerName,dateStr,dateStr2,_developerName,classNameStr];
+    [self.sourceString appendFormat:@"#import \"%@URL.h\"",_className];
+    [self.sourceString appendString:@"\n\n"];
+}
 
 - (NSString *)appendBlockInfo{
     return @"Success:(SuccessBlock)successBlock failure:(FailureBlock)failure netError:(ErrorBlock)errorBlock;";
+}
+- (NSString *)appendBlockInfoSource{
+    return @"Success:(SuccessBlock)successBlock failure:(FailureBlock)failure netError:(ErrorBlock)errorBlock";
 }
 
 /**
@@ -139,37 +215,15 @@
     }
 
     BOOL headFileFlag = NO;
+    BOOL sourceFileFlag = NO;
     NSString *headFilePath = [dirPath stringByAppendingPathComponent:[NSString stringWithFormat:@"%@%@.h",_className,name]];
     headFileFlag = [self.headerString writeToFile:headFilePath atomically:NO encoding:NSUTF8StringEncoding error:nil];
+
+    NSString *sourceFilePath = [dirPath stringByAppendingPathComponent:[NSString stringWithFormat:@"%@%@.m",_className,name]];
+    sourceFileFlag =  [self.sourceString writeToFile:sourceFilePath atomically:NO encoding:NSUTF8StringEncoding error:nil];
     return headFileFlag;
 }
 
-
-/**
- * 生成文件并存放到指定的目录下
- * @return 成功为YES 失败为NO
- * deprecated 目前弃用
- */
-- (BOOL)generateFileAllowForSource:(NSString *)name
-
-{
-    NSArray *paths = NSSearchPathForDirectoriesInDomains(NSDocumentDirectory,NSUserDomainMask,YES);
-    NSString *dirPath = [paths[0] stringByAppendingPathComponent:@"ClassFiles"];
-
-    NSFileManager *fm = [NSFileManager defaultManager];
-    [fm removeItemAtPath:dirPath error:nil];
-    BOOL dir = NO;
-    BOOL exis = [fm fileExistsAtPath:dirPath isDirectory:&dir];
-    if (!exis && !dir)
-    {
-        [fm createDirectoryAtPath:dirPath withIntermediateDirectories:NO attributes:nil error:nil];
-    }
-
-    BOOL sourceFileFlag = NO;
-    NSString *sourceFilePath = [dirPath stringByAppendingPathComponent:[NSString stringWithFormat:@"%@.m",_className]];
-    sourceFileFlag =  [self.sourceString writeToFile:sourceFilePath atomically:NO encoding:NSUTF8StringEncoding error:nil];
-    return sourceFileFlag;
-}
 
 
 /**
@@ -305,7 +359,8 @@
     NSMutableString *parmsString = [NSMutableString string];
     [parmsString appendString:[self addMarkHeader]];
     for (int i = 0; i < marksModels.count; i++) {
-        [self addMarkModel:marksModels[i]];
+        SHParmsModel *parmsModel = marksModels[i];
+        [parmsString appendString:[self addMarkBodyName:parmsModel.netParameterName markName:parmsModel.netNoteName]];
     }
     [parmsString appendString:[self addMarkFooter]];
     return parmsString;
@@ -406,6 +461,31 @@
     }
 
     return dict;
+}
+
+/**在一个字符串外部加一对大括号**/
+- (NSMutableString *)addBrackets:(NSString *)sourceString {
+    NSMutableString *str = [NSMutableString string];
+    [str appendString:@"{\n\n"];
+    [str appendString:sourceString];
+    [str appendString:@"\n}\n"];
+    return str;
+}
+
+/**在一个字符串外部加一对小括号**/
+- (NSMutableString *)addParentheses:(NSString *)sourceString {
+    NSMutableString *str = [NSMutableString string];
+    [str appendString:@"@("];
+    [str appendString:sourceString];
+    [str appendString:@")"];
+    return str;
+}
+
+- (NSMutableString *)addUnifiedUrl:(NSString *)Url requestType:(NSString *)requestType{
+
+    NSMutableString *tempStr = [NSMutableString string];
+    [tempStr appendFormat:@" [MTHttpRequest_Helper %@RequestWithURL:%@ parameters:params success:^(id responseObject) {\n[self judgeReturnValueResponseObject:responseObject Success:^{\nif (successBlock) {\nsuccessBlock(responseObject);\n}\n} fauile:^{\nif (failure) {\nfailure(responseObject);\n}\n}];\n} failure:^(NSError *error, NSString *errorStr) {\nif (errorBlock) {\nerrorBlock(error);\n}\n}];",requestType,Url];
+    return tempStr;
 }
 
 @end
